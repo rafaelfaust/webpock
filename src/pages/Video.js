167bc4defab924d57ec4a183e3fe22e8165162f3
Webfael <106990982+Webfael@users.noreply.github.com>
2023-03-30 22:36:59 -0300

    video

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,37 +1,33 @@
-import React, { useRef, useState } from 'react'
-import VideoFooter from './components/footer/VideoFooter'
-import './video.css'
+import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter";
+import "./video.css";
 
 function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
 
-    const videoRef = useRef(null);
-    const [play, setPlay] = useState(false);
-
-    function hanndleStart (){
- 
-        if(play){
-            videoRef.current.pause();
-            setPlay(false);
-
-        }  else {
-            videoRef.current.play();
-            setPlay(true);
-        }  
+  function hanndleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
     }
+  }
 
   return (
-    <div className='video'>
-        <video 
-            className='video_player'
-            ref={videoRef}
-            onClick={hanndleStart}
-            loop
-            src='https://static.videezy.com/system/resources/previews/000/018/948/original/ICON-VERSION8_1.mp4'
-        >
-        </video>
-        <VideoFooter/>
+    <div className="video">
+      <video
+        className="video_player"
+        ref={videoRef}
+        onClick={hanndleStart}
+        loop
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+      ></video>
+      <VideoFooter />
     </div>
-  )
+  );
 }
 
-export default Video
+export default Video;
